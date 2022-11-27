4a327134850bd0a32f67198325787c84a5b9ac6e
Martin Aborgeh <aborgehmartin>
2022-11-27 15:31:18 +0000

    Render Deploy

--- GeoAppWebProject/settings.py
+++ GeoAppWebProject/settings.py
@@ -12,8 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import os
-import django_heroku
-import dj_database_url
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -145,7 +144,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
 STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
-django_heroku.settings(locals())
+
 MEDIA_URL='/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media/')
 
